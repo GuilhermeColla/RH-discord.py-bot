e9356cb9c3a13b420c9f7d2a3eeed5cd46a71e0c
GuilhermeColla <guilherme.colla.159@gmail.com>
2021-08-03 12:03:45 -0300

    Novo sistema de Load/unload de extensões (cogs).

---
+++ lib/cogs/manual_load_unload.py
@@ -0,0 +1,35 @@
+from discord.ext.commands import Cog, command
+
+""" 
+Implementa um sistema para carregar e descarrregar Cogs.
+Isso possibilita que cogs possam ser modificados e atualizados com o bot em
+funcionamento (restartless patches).
+"""
+
+
+class ManualLoadUnload(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    # Event
+    @Cog.listener()
+    async def on_ready(self):
+        print(f"{__name__} cog ready")
+    
+
+    # Commands
+    @command()
+    async def load(self, ctx, extension):
+        self.bot.load_extension(f"lib.cogs.{extension}")
+        print(f"Loaded {extension}")
+
+    @command()
+    async def unload(self, ctx, extension):
+        self.bot.unload_extension(f"lib.cogs.{extension}")
+        print(f"Unloaded {extension}")
+
+
+
+
+def setup(bot):
+    bot.add_cog(ManualLoadUnload(bot))
